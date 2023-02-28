84ae2113dadcf2fbc12d5b0bb24356ac1cadb79e
jhon-stack <jgaitanaguilar@gmail.com>
2023-02-27 18:12:05 -0600

    En el archivo lab_5_axis.ino el tiempo se estaba imprimiendo en milisegundos, lo que hice fue dividirlo entre mil y punto flotante para que sea mas exacto rl contador que un entero. Se agrega el archivo generador de cvs's, estan parametrizados: 1. la cantidad de muestras que se quieren. 2. El nombre del archivo que se quiera crear en funcion del movimiento.

---
+++ src/axis_csv_gen.py
@@ -0,0 +1,45 @@
+
+import serial
+import csv
+import os
+
+
+#cambiar aqui el nombre del puerto (debe coincidir con el que esta en Arduino IDE)
+puerto = "/dev/ttyACM0"
+#serializamos
+#En port ponemos el valor que se obtiene al ejeciutar el script virtualport.sh
+ser = serial.Serial(port = puerto,baudrate=9600,parity=serial.PARITY_NONE,stopbits=serial.STOPBITS_ONE,bytesize=serial.EIGHTBITS) 
+#ser = serial.Serial(port = '/dev/pts/3',baudrate=9600,parity=serial.PARITY_NONE,stopbits=serial.STOPBITS_ONE,bytesize=serial.EIGHTBITS) 
+current_directory = os.getcwd()
+#cambiar el segundo argumento por alguno de estos cuando se esten guardando los datos:
+'''
+  1. Mover el brazo hacia arriba    ->  brazo_arriba.csv
+  2. Mover el brazo hacia abajo     ->  brazo_abajo.csv
+  3. Mover el brazo hacia al frente ->  golpe.csv
+'''
+#Asegurarse que el dump file quede dentro del mismo dir
+csv_path = os.path.join(current_directory, "dummy.csv")
+#pequena tabla para ver la duracion de las interaciones
+'''
+Iteraciones   tiempo(s)
+1500          ~13  
+1000          ~9.2
+'''
+with open(csv_path, mode ='w') as csv_file:
+  writer = csv.writer(csv_file)
+  writer.writerow(['Timestamp', 'X', 'Y', 'Z'])
+  #cambiar el valor muestras por el valor de muestras que se desean
+  muestras = 1000
+  cnt = 0
+  while True:
+    line = ser.readline().decode().strip()
+    data = line.split('\t')
+    timestamp = int(data[0])
+    x = float(data[1])
+    y = float(data[2])
+    z = float(data[3])
+    writer.writerow([timestamp, x, y, z])
+    print(f"Timestamp: {timestamp}, X: {x}, Y: {y}, Z: {z}")
+    cnt += 1
+    if cnt >= muestras:
+      break
---
+++ src/lab_5_axis/lab_5_axis.ino
@@ -0,0 +1,44 @@
+/*
+Este modulo se encarga de leer los movimientos del giroscopio del Arduino
+Nano 33 BLE Sense Lite y escribir esas lecturas en varios csv's a segun el movimiento.
+Los movimientos son 3: 
+1. Mover el brazo hacia arriba    ->  brazo_arriba.csv
+2. Mover el brazo hacia abajo     ->  brazo_abajo.csv
+3. Mover el brazo hacia al frente ->  golpe.csv
+Dichos movimientos se registran en otro script de Python que guarda esos valores via usb. 
+El scrit se llama axis_csv_gen.py. El nombre que se le asigna a cada archivo que guarda son los mencionados
+anteriormente
+*/
+#include <LSM9DS1.h>
+
+void setup() {
+  Serial.begin(9600);
+	while (!Serial);
+	//Serial.println("Started");
+  if (!IMU.begin()) {
+    //Serial.println("Failed to initialize IMU!");
+		while (1);
+	}
+	//Serial.print("Gyroscope sample rate = ");
+	//Serial.print(IMU.gyroscopeSampleRate());
+	//Serial.println(" Hz");
+	//Serial.println();
+	//Serial.println("Gyroscope in degrees/second");
+	//Serial.println("\tTimestamp\tX\tY\tZ");
+}
+
+void loop() {
+  float x, y, z;
+	unsigned long timestamp = millis();
+	if (IMU.gyroscopeAvailable()) {
+    IMU.readGyroscope(x, y, z);
+		//Serial.print('\t');    
+		Serial.print(timestamp/1000.00);
+		Serial.print('\t');    
+		Serial.print(x);
+		Serial.print('\t');
+		Serial.print(y);
+		Serial.print('\t');
+		Serial.println(z);
+	}
+}
